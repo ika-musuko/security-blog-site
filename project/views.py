274272b405858ed3503c7d0a42ccceddf63402af
sherwyn <sherwyn.sen@gmail.com>
2018-04-29 21:28:15 -0700

    laid out views

--- project/views.py
+++ project/views.py
@@ -1,5 +1,39 @@
 from project import app
 
+from flask import redirect, url_for
+
+
 @app.route('/')
 def index():
     return "hi"
+
+
+@app.route("/demos")
+def demos():
+    return "demos"
+
+
+@app.route("/login")
+def login():
+    return "login"
+
+
+@app.route("/register")
+def register():
+    return "register"
+
+
+@app.route("/logout")
+def logout():
+    redirect(url_for("index"))
+
+
+@app.route("/post/<int:number>")
+def post(number: int):
+    return "post: %s" % number
+
+
+@app.route("/privacy")
+def privacy():
+    return "privacy statement"
+
