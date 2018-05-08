5628e6ad8b1e4cf02d8d48d8f60213ba7143a56e
sherwyn <sherwyn.sen@gmail.com>
2018-05-08 05:33:51 -0700

    moved fake post generator to utils, added "edit own post" bonus item

--- project/views/normal_views.py
+++ project/views/normal_views.py
@@ -5,29 +5,11 @@ routes for the normal views
 '''
 
 from flask import redirect, url_for, render_template
-import string
-import random
 import datetime
 
-from project import app, POSTS_PER_PAGE
+from project import app
+from project.views.view_utils import postmaker
 
-def random_string(length: int):
-    return ''.join([random.choice(string.ascii_lowercase) for _ in range(length)])
-
-def postmaker(total_posts: int, page: int):
-    total_posts = 41
-
-    posts = [{
-        "id": i+1
-        ,"title": random_string(8)
-        ,"author": random_string(13)
-        ,"published_on": datetime.datetime.today()
-        ,"preview": random_string(random.randint(50,125))
-    } for i in range(total_posts)]
-
-    # array indexing starts on 0
-    ppage = page - 1
-    return posts[ppage*POSTS_PER_PAGE:page*POSTS_PER_PAGE]
 
 @app.route('/')
 @app.route('/index/')
--- project/views/view_utils.py
+++ project/views/view_utils.py
@@ -1,5 +1,29 @@
+import datetime
+import random
+import string
+
 from project import app, POSTS_PER_PAGE
 
 @app.context_processor
 def inject_posts_per_page():
     return dict(POSTS_PER_PAGE=POSTS_PER_PAGE)
+
+
+def random_string(length: int):
+    return ''.join([random.choice(string.ascii_lowercase) for _ in range(length)])
+
+
+def postmaker(total_posts: int, page: int):
+    total_posts = 41
+
+    posts = [{
+        "id": i+1
+        ,"title": random_string(8)
+        ,"author": random_string(13)
+        ,"published_on": datetime.datetime.today()
+        ,"preview": random_string(random.randint(50,125))
+    } for i in range(total_posts)]
+
+    # array indexing starts on 0
+    ppage = page - 1
+    return posts[ppage*POSTS_PER_PAGE:page*POSTS_PER_PAGE]
